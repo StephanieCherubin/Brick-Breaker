033b3b7fda60a3874e212b6208efdf87632994fd
Stephanie Cherubin <seraphin.cherubin@gmail.com>
2020-02-11 21:45:51 -0800

    add new function for hue/saturation/color random

--- js/script.js
+++ js/script.js
@@ -1,15 +1,24 @@
 /* eslint-disable no-alert */
 /* eslint-disable no-undef */
 
+// hex code random color
 function randomColor() {
   return `#${Math.floor(Math.random() * 0xffffff).toString(16)}`;
 }
 
+// rgb random color
 function differentColor() {
   return `rgb(
     ${Math.floor(256 * Math.random())},
     ${Math.floor(256 * Math.random())},
     ${Math.floor(256 * Math.random())})`;
+}
+
+// hue saturation lightness random color
+function randomHSL(s, l) {
+  const h = Math.floor(Math.random() * 360)
+
+  return `hsl(${h}, ${s}%, ${l}%)`
 }
 
 const game = new Game();
